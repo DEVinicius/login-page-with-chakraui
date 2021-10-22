a2b1fb0a1265fee97bf5c816421f1f89362830c2
Vinicius Pereira de Oliveira <agavi2014@hotmail.com>
2021-10-21 23:27:33 -0300

    [NEW] First COmmit

---
+++ src/components/login.tsx
@@ -0,0 +1,14 @@
+import { Image } from "@chakra-ui/image";
+import { Flex } from "@chakra-ui/layout";
+
+export function Login() {
+    return (
+        <Flex>
+            <Image 
+                src="https://cdn.pixabay.com/photo/2017/01/16/17/05/whatsapp-1984584_960_720.png"
+                alt="TESTE"
+                title="Image logo"
+            />
+        </Flex>
+    )
+}
